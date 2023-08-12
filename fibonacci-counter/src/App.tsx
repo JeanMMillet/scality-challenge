0150cd282cce4e060d8748e20483c853a6b0c902
JeanMMillet <millet.jm@live.com>
2023-08-13 01:12:56 +0200

    add reset and settings button, handlereset function

--- fibonacci-counter/src/App.tsx
+++ fibonacci-counter/src/App.tsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
-import { AiOutlineRight } from "react-icons/ai";
+import {
+  AiOutlineRight,
+  AiOutlineSetting,
+  AiOutlineUndo,
+} from "react-icons/ai";
 import "./App.css";
 import CounterButton from "./Component/CounterButton/CounterButton";
 import ProgressionBar from "./Component/ProgressionBar/ProgressionBar";
+import AlertBox from "./Component/AlertBox/AlertBox";
 
 function App() {
   const [counter, setCounter] = useState(0);
+
+  const handleResetClick = () => {
+    setCounter(0);
+  };
 
   return (
     <div className="app">
       <h1 className="title">Fibonacci Indicator</h1>
       <div className="sub-title">
         <h2>
-          This app indicates if the number is includes in the Fibonacci sequence
+          This app indicates if the number is included in the Fibonacci sequence
         </h2>
         <button type="button" className="learn-more">
           Learn more
@@ -39,13 +48,25 @@ function App() {
         />
       </div>
       <div className="progression-bar">
-        <p>Barre de progression</p> <br />
-        <p>alert box</p>
         <ProgressionBar counter={counter} max={10} />
+        <AlertBox
+          counter={counter}
+          max={10}
+          step={1}
+          visible={counter === 10 - 2}
+        />
       </div>
       <div className="utilities">
-        <p>Reset icon</p>
-        <p>Settings icon</p>
+        <button
+          type="button"
+          onClick={handleResetClick}
+          style={counter === 0 ? { opacity: "50%" } : undefined}
+        >
+          <AiOutlineUndo />
+        </button>
+        <button type="button">
+          <AiOutlineSetting />
+        </button>
       </div>
     </div>
   );
