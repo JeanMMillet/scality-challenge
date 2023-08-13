54dd525089ff7c454650c9f8195e3406b2d10bf5
JeanMMillet <millet.jm@live.com>
2023-08-13 21:34:57 +0200

    test keydown

--- fibonacci-counter/src/Component/CounterButton/CounterButton.tsx
+++ fibonacci-counter/src/Component/CounterButton/CounterButton.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 import "./CounterButton.css";
+import { useEffect, useRef } from "react";
 
 interface CounterButtonProps {
   setter: React.Dispatch<React.SetStateAction<number>>;
@@ -16,6 +17,59 @@ const CounterButton: React.FC<CounterButtonProps> = ({
   max,
   counter,
 }) => {
+  const callTimeout = useRef(true);
+  const canCallTheFunction = useRef(true);
+
+  useEffect(() => {
+    function manageArrow(e: KeyboardEvent) {
+      console.log(e.key, canCallTheFunction.current, callTimeout.current);
+      if (e.key === "ArrowRight") {
+        // when we arrive here trigger the timer
+
+        console.log("canCallTheFunction.current", canCallTheFunction.current);
+
+        if (callTimeout.current === true) {
+          console.log("in if", callTimeout.current);
+          callTimeout.current = false;
+          console.log("after set", callTimeout.current);
+          setTimeout(() => {
+            // console.log(
+            //   "canCallTheFunction set timeout",
+            //   canCallTheFunction.current
+            // );
+
+            setter((prevState) => prevState + step);
+            canCallTheFunction.current = true;
+            callTimeout.current = true;
+            console.log("after set 2", callTimeout.current);
+          }, 1000);
+        }
+
+        canCallTheFunction.current = false;
+
+        // change canCallTheFunction to false
+        // after 1sec change it back to true
+
+        // if canCallTheFunction is true
+        // call the function
+        // if (canCallTheFunction.current === true) {
+        //   setter((prevState) => prevState + step);
+        // }
+        // setter((prevState) => prevState + step);
+      }
+      //  else if (e.key === "ArrowLeft") {
+      //   // when we arrive here
+      //   setter((prevState) => prevState - step);
+      // }
+    }
+
+    window.addEventListener("keydown", manageArrow);
+
+    return () => {
+      window.removeEventListener("keydown", manageArrow);
+    };
+  }, [callTimeout.current]);
+
   const handleClick = () => {
     if (type === "increment" && counter + step <= max) {
       setter((prevState) => prevState + step);
